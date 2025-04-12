9945905c19404ea17547288cb82d277462dc2461
sagar <sagarsocia9@gmail.com>
2025-04-12 10:35:27 +0530

    added forms

---
+++ adminto/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import PasswordChangeForm
+
+class MyProfileForm(forms.ModelForm):
+    password = forms.CharField(
+        label='New Password',
+        widget=forms.PasswordInput,
+        required=False,  # Make it optional
+        help_text='Leave blank if you do not want to change your password.'
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ['full_name', 'password', 'email', 'profile_file']  # Add other fields as necessary
+
+    def __init__(self, *args, **kwargs):
+        super(MyProfileForm, self).__init__(*args, **kwargs)
+        self.fields['email'].disabled = True  # Disable email field
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        # Check if both password fields are filled and match
+        if password and confirm_password and password != confirm_password:
+            self.add_error('confirm_password', "Passwords do not match.")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super(MyProfileForm, self).save(commit=False)
+        if self.cleaned_data['password']:
+            user.set_password(self.cleaned_data['password'])  # Hash the password
+        if commit:
+            user.save()
+        return user
+
+class PageForm(forms.Form):
+    title = forms.CharField(max_length=100, required=True)
+    type_id = forms.ChoiceField(choices=[(1, 'Terms of Service'), (2, 'Privacy Policy'), (3, 'About Us'), (4, 'FAQ')], required=True)
+    description = forms.CharField(required=True)

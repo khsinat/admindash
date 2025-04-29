789f8122150b82f6855cd3abf397b4fc024ea694
sagar <sagar>
2025-04-29 19:02:02 +0000

    d

--- api/serializers.py
+++ api/serializers.py
@@ -336,9 +336,3 @@ class AnalysisSerializerResolver(serializers.ModelSerializer):
             'image_file'  # Include the image URL in the fields
         ]
         read_only_fields = ['image_file']  # Make image_url read-only
-
-    def get_image_file(self, obj):
-        # Assuming the image_file field is a list of images, return the URL of the first image
-        if obj.image_file:
-            return obj.image_file 
-        return []
